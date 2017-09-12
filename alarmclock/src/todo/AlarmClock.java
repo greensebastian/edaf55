6c391c40c5b847c34ff04e8891abe6e5241290fd
Sebastian Green <Sebastian Green>
2017-09-12 18:47:38 +0200

    Created datahandler and timehandler

--- alarmclock/src/todo/AlarmClock.java
+++ alarmclock/src/todo/AlarmClock.java
@@ -1,5 +1,6 @@
 package todo;
 import done.*;
+import se.lth.cs.realtime.semaphore.MutexSem;
 import se.lth.cs.realtime.semaphore.Semaphore;
 
 /**
@@ -12,8 +13,12 @@ public class AlarmClock {
 
 	private ClockInput	input;
 	private ClockOutput	output;
-	private Semaphore	signal; 
+	private Semaphore	signal;
 	// Declare thread objects here..
+	
+	private SharedData data;
+	private TimeHandler time;
+	private ButtonHandler btnHandler;
 
 	/**
 	 * Create main application and bind attributes to device drivers.
@@ -40,12 +45,18 @@ public class AlarmClock {
 	public void start() {
 		// Delete/replace the following test/demo code;
 		// make something happen by exercising the IO:
+		
+		data = new SharedData();
+		btnHandler = new ButtonHandler();
+		
+		time = new TimeHandler();
+		time.start();
 
 		// Create thread objects here...
-		Thread removeMeFromApplication = new Thread(new InputOutputTest());
+		/*Thread removeMeFromApplication = new Thread(new InputOutputTest());
 		
 		// Create threads of execution by calling start...
-		removeMeFromApplication.start(); 
+		removeMeFromApplication.start(); */
 	}
 	
 	class InputOutputTest implements Runnable {
@@ -66,5 +77,88 @@ public class AlarmClock {
 		}
 
 	}
+	
+	class ButtonHandler extends Thread {
+		public void run() {
+			while(!Thread.currentThread().isInterrupted()){
+				input.getSemaphoreInstance().take();
+				int choice = input.getChoice();
+				switch (choice) {
+					case ClockInput.SHOW_TIME: {
+						break;
+					}
+					case ClockInput.SET_ALARM: {
+						break;
+					}
+					case ClockInput.SET_TIME: {
+						break;
+					}
+				}
+			}
+		}
+	}
+	
+	class TimeHandler extends Thread {
+		public void run() {
+			while(!Thread.currentThread().isInterrupted()){
+				
+				long curr = System.currentTimeMillis();
+				data.incTime();
+				try {
+					long dt = 1000-curr%1000;
+					sleep(dt);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+	
+	class SharedData {
+		private int seconds;
+		private int minutes;
+		private int hours;
+		private MutexSem mutex;
+		
+		public SharedData(){
+			mutex = new MutexSem();
+			mutex.take();
+			seconds = 0;
+			minutes = 0;
+			hours = 0;
+			mutex.give();
+		}
+		
+		private void setTime(int hhmmss){
+			mutex.take();
+			hours = hhmmss/10000;
+			minutes = (hhmmss - 10000*hours)/100;
+			seconds = (hhmmss - 10000*hours - 100*minutes);
+			mutex.give();
+		}
+		
+		private void incTime(){
+			mutex.take();
+			seconds++;
+			if (seconds >= 60){
+				seconds = 0;
+				minutes++;
+			}
+			if (minutes >= 60){
+				minutes = 0;
+				hours++;
+			}
+			if (hours >= 24){
+				hours = 0;
+			}
+			mutex.give();
+		}
+		
+		private int getTime(){
+			return 10000*hours + 100*minutes + seconds;
+		}
+	}
+	
+	
 	
 }
